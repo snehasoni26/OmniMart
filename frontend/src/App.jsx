96160bb6f2da9fb6aec6d3010c5660f8f3f4d8fd
manvibhutani <125116772+manvibhutani@users.noreply.github.com>
2024-08-12 02:18:41 +0530

    y

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './globals.css'
+import { ThreeDCardDemo } from './components/test/3d-card-demo'
+import { TypewriterEffectDemo } from './components/test/typewriter-effect-demo'
+import HeroSection from './components/ui/hero-section'
+import {
+  BrowserRouter, Routes, Route,
+} from 'react-router-dom';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='h-full w-full'>
+   <HeroSection></HeroSection>
+   
+   </div>
+      </>
+  )
+}
+
+export default App
