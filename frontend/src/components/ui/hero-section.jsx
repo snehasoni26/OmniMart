14bd8e5a22d34816ec2767abeb5ea5bee84e160f
manvibhutani <125116772+manvibhutani@users.noreply.github.com>
2024-08-12 22:32:45 +0530

    fixing css

--- frontend/src/components/ui/hero-section.jsx
+++ frontend/src/components/ui/hero-section.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {
     BrowserRouter, Routes, Route,
   } from 'react-router-dom';
-  import AdminRoutes from './admin/adminRoutes';
+
   
 function HeroSection() {
   return (
