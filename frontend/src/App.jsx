33c1ec47f957810a8a683a95ee4f61097252ff14
manvibhutani <125116772+manvibhutani@users.noreply.github.com>
2024-08-14 00:47:55 +0530

    setting up router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,32 @@ import './globals.css'
 import { ThreeDCardDemo } from './components/test/3d-card-demo'
 import { TypewriterEffectDemo } from './components/test/typewriter-effect-demo'
 import HeroSection from './components/ui/hero-section'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
 import {
-  BrowserRouter, Routes, Route,
-} from 'react-router-dom';
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <TypewriterEffectDemo/>,
+  },
+  {
+    path:"/manvi",
+    element:<div>HIi</div>
+  }
+]);
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
 
 function App() {
   const [count, setCount] = useState(0)
-
+  
   return (
     <>
     <div className='h-full w-full'>
--- frontend/src/components/test/typewriter-effect-demo.jsx
+++ frontend/src/components/test/typewriter-effect-demo.jsx
@@ -19,8 +19,8 @@ export function TypewriterEffectDemo() {
   ];
   return (
     <div className="flex flex-col items-center justify-center h-[40rem] ">
-      <p className="text-neutral-600 dark:text-neutral-200 text-base  mb-10">
-        The road to freedom starts from here
+      <p className="text-neutral-600 dark:text-neutral-200 text-lg  mb-10">
+        Bringing out virtual experience to life
       </p>
       <TypewriterEffect words={words} />
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-10">
@@ -28,7 +28,7 @@ export function TypewriterEffectDemo() {
   Join now
 </button>
 
-        <button className="w-40 h-10 text-black rounded-xl bg-white border dark:border-white border-transparent text-white text-sm shadow-lg shadow-blue-500/50 hover:shadow-blue-500/100 transition">
+        <button className="w-40 h-10 text-black rounded-xl bg-white border dark:border-white border-transparent text-sm shadow-lg shadow-blue-500/50 hover:shadow-blue-500/100 transition">
           Signup
         </button>
       </div>
